d19fe6e240a081b1f56fc886b4104659bed1aa6e
Felipe Velasco <felipe.velasco@baaskit.com>
2023-07-10 16:08:50 -0400

    se modifica .gitignore

--- vpc.d.ts
+++
@@ -1,8 +0,0 @@
-import { Construct } from 'constructs';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-export declare class VpcPrivate extends ec2.Vpc {
-    constructor(scope: Construct, id: string, props?: ec2.VpcProps);
-}
-export declare class VpcPrivateInternet extends ec2.Vpc {
-    constructor(scope: Construct, id: string, props?: ec2.VpcProps);
-}
